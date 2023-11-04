1a60f7e41d8e435f8bd854b8a6874c3b56c54a7b
mustafozoda <129182857+mustafozoda@users.noreply.github.com>
2023-11-04 21:53:13 +0100

    .

--- test.py
+++ test.py
@@ -1,39 +1,39 @@
-import PyPDF2
-from gtts import gTTS
+# import PyPDF2
+# from gtts import gTTS
 
 
 
 
-# Function to extract text from a PDF file
-def extract_text_from_pdf(pdf_path):
-    text = ""
-    pdf_file = open(pdf_path, 'rb')
-    pdf_reader = PyPDF2.PdfReader(pdf_file)
+# # Function to extract text from a PDF file
+# def extract_text_from_pdf(pdf_path):
+#     text = ""
+#     pdf_file = open(pdf_path, 'rb')
+#     pdf_reader = PyPDF2.PdfReader(pdf_file)
     
-    for page_num in range(len(pdf_reader.pages)):
-        page = pdf_reader.pages[page_num]
-        text += page.extract_text()
+#     for page_num in range(len(pdf_reader.pages)):
+#         page = pdf_reader.pages[page_num]
+#         text += page.extract_text()
     
-    pdf_file.close()
-    return text
+#     pdf_file.close()
+#     return text
 
 
 
 
-# Function to convert text to speech and save it as an MP3 file
-def text_to_mp3(text, mp3_file):
-    tts = gTTS(text, lang='en')
-    tts.save(mp3_file)
+# # Function to convert text to speech and save it as an MP3 file
+# def text_to_mp3(text, mp3_file):
+#     tts = gTTS(text, lang='en')
+#     tts.save(mp3_file)
 
 
 
 
-# Main function
-if __name__ == "__main__":
-    pdf_path = "D:/Projects/Python Projects/PDF_To_mp3/CV.pdf"
-    mp3_file = "output_audio.mp3"
+# # Main function
+# if __name__ == "__main__":
+#     pdf_path = "D:/Projects/Python Projects/PDF_To_mp3/CV.pdf"
+#     mp3_file = "output_audio.mp3"
     
-    text = extract_text_from_pdf(pdf_path)
-    text_to_mp3(text, mp3_file)
+#     text = extract_text_from_pdf(pdf_path)
+#     text_to_mp3(text, mp3_file)
     
-    print(f"Conversion completed. MP3 file saved as {mp3_file}")
+#     print(f"Conversion completed. MP3 file saved as {mp3_file}")
---
+++ test_2.py
@@ -0,0 +1,30 @@
+import PyPDF2
+from gtts import gTTS
+
+
+def pdf_to_text(pdf_link):
+    text_from_pdf = ""
+    pdf_file = open(pdf_link, 'rb')
+    pdf_reader = PyPDF2.PdfReader(pdf_file)
+
+    for page_numbers in range(len(pdf_reader.pages)):
+        page = pdf_reader.pages[page_numbers]
+        text_from_pdf += page.extract_text()
+
+    pdf_file.close()
+    return text_from_pdf
+
+
+def text_to_mp3(text, mp3_file):
+    speach_from_text = gTTS(text, lang='en')
+    speach_from_text.save(mp3_file)
+
+
+if __name__ == "__main__":
+    pdf_link = "D:/Projects/Python Projects/PDF_To_mp3/text.pdf"
+    mp3_file = "audio.mp3"
+
+    text = pdf_to_text(pdf_link)
+    text_to_mp3(text, mp3_file)
+
+    print(f"Well done, Your mp3 file is ready")
